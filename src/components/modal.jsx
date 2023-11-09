8195c12009f54fe535a2e9d2c470ad022af6e551
dlin <lindevin101@gmail.com>
2023-11-09 11:35:51 -0500

    finished

---
+++ src/components/modal.jsx
@@ -0,0 +1,17 @@
+export default function Modal({poke, pokeInfo}) {
+
+    let pokeName = poke.name.charAt(0).toUpperCase() + poke.name.slice(1);
+    return (
+        <div className="modal__content">
+            <h2 className="name">{pokeName}</h2>
+            <img className="picture" src={poke.sprites.front_default} /> 
+            <p className="info">
+                {pokeInfo}
+            </p>
+            <div className="modal__footer">
+            </div>
+
+            <a href="#" className="modal__close">&times;</a>
+        </div>
+    )
+}
